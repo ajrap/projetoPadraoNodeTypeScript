0890ba1342afdeef2cb86a13b594da748d2b11eb
andre2308 <andre2308@gmail.com>
2025-08-22 19:23:33 -0300

    Persistencia da estrutura do backend

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+
+import express, { Request, Response, NextFunction } from 'express'
+
+import { router } from './routes'
+const app = express();
+
+app.listen(3333, () => {
+  console.log('Server is running on port 3333');
+});
+
