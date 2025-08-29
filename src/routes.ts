8ea15dd39db9a60c6f2c575e67a8669f4e68ec2a
andre2308 <andre2308@gmail.com>
2025-08-29 17:32:32 -0300

    Alterando Server e Routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+    throw new Error('This is a test error')
+});
+
+router.get('/go', (req: Request, res: Response) => {
+    return res.json({ message: 'Go route is working!' });
+});
+
+
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,35 @@
 
 import express, { Request, Response, NextFunction } from 'express'
-
+import 'express-async-errors';
 import { router } from './routes'
-const app = express();
 
-app.listen(3333, () => {
-  console.log('Server is running on port 3333');
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+//variáveis 
+const port = process.env.PORT || 3334;
+
+//middleware de tratamento de erros
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+  //se for um erro conhecido
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+
+
+ //se for um erro desconhecido
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
